c6f929432acb34067f232d202b317ce5732e5466
Mayank Baiswar <mbaiswar@atlassian.com>
2022-06-12 01:34:39 +0530

    * added display for address

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Popover } from '@headlessui/react';
 import Web3 from 'web3/dist/web3.min.js';
 
 export default function App() {
@@ -10,42 +9,41 @@ export default function App() {
 
     const requestAccount = async () => {
         const { ethereum } = window;
-        if(!ethereum) {
-            alert('Please install MetaMask');
+        if (!ethereum) {
+            alert('Please install MetaMask!');
         }
         try {
             const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
             console.log(accounts);
             setAccount([accounts[0]]);
-        }catch(err) {
+        } catch (err) {
             console.log(err);
         }
     }
 
     return (
-        <Popover className="relative bg-white">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6">
-                <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
-                    <div className="flex justify-start lg:w-0 lg:flex-1">
-                        <a href="#">
-                            <span className="sr-only">Web3</span>
-                            <img
-                                className="h-8 w-auto sm:h-10"
-                                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                                alt=""
-                            />
-                        </a>
-                    </div>
+        <div className="max-w-7xl mx-auto px-4 sm:px-6">
+            <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+                <div className="flex justify-start lg:w-0 lg:flex-1">
+                    <a href="#">
+                        <span className="sr-only">Web3</span>
+                        <img
+                            className="h-8 w-auto sm:h-10"
+                            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                            alt=""
+                        />
+                    </a>
+                </div>
 
-                    <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-                        <button className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-                            onClick={requestAccount}
-                        >
-                            Connect wallet
-                        </button>
-                    </div>
+                <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+                    {`${account ? account : 'No account'}`}
+                    <button className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                        onClick={account ? undefined : requestAccount}
+                    >
+                        Connect wallet
+                    </button>
                 </div>
             </div>
-        </Popover>
+        </div>
     )
 }
